41dd139242544fc0387117ce43ed31f346a99a7c
Jim Burlison <JBurlison@Gmail.com>
2019-07-22 04:51:25 -0400

    Added log message

--- Pandaros.SimpleAnitgrief/Permission.cs
+++ Pandaros.SimpleAnitgrief/Permission.cs
@@ -13,10 +13,15 @@ namespace Pandaros.SimpleAnitgrief
         [ModLoader.ModCallback(ModLoader.EModCallbackType.OnTryChangeBlock, "Pandaros.SimpleAntigrief.Permission.OnTryChangeBlockUser")]
         public static void OnTryChangeBlockUser(ModLoader.OnTryChangeBlockData userData)
         {
-            if (ServerManager.BlockEntityTracker.BannerTracker.TryGetClosest(userData.Position, out BannerTracker.Banner existingBanner, ServerManager.ServerSettings.Colony.ExclusiveRadius * 2))
+            if (ServerManager.BlockEntityTracker.BannerTracker.TryGetClosest(userData.Position, out BannerTracker.Banner existingBanner, ServerManager.ServerSettings.Colony.ExclusiveRadius))
             {
-                if (userData.RequestOrigin.Type == BlockChangeRequestOrigin.EType.Player && !existingBanner.Colony.Owners.Contains(userData.RequestOrigin.AsPlayer))
+                if (userData.RequestOrigin.Type == BlockChangeRequestOrigin.EType.Player &&
+                    userData.RequestOrigin.AsPlayer.ID.type == NetworkID.IDType.Steam &&
+                    !PermissionsManager.HasPermission(userData.RequestOrigin.AsPlayer, new PermissionsManager.Permission("god")) &&
+                    !existingBanner.Colony.Owners.Contains(userData.RequestOrigin.AsPlayer))
                 {
+                    Chatting.Chat.Send(userData.RequestOrigin.AsPlayer, "You may not build near this colony. Maybe you can ask one of the owners for an invite! The owners are: " + string.Join(", ", existingBanner.Colony.Owners.Select(o => o.Name)));
+                    userData.InventoryItemResults.Clear();
                     userData.CallbackState = ModLoader.OnTryChangeBlockData.ECallbackState.Cancelled;
                     userData.CallbackConsumedResult = EServerChangeBlockResult.CancelledByCallback;
                 }
